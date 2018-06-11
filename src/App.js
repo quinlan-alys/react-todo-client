370daea5b93c587491f4a2be78d15b9f67a22724
Quinlan Deer <quinlan.deer@gmail.com>
2018-06-11 13:59:37 -0400

    create component and input detection and mapping

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: '',
+            items: []
+        }
+
+        this.inputChange = this.inputChange.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('/index')
+            .then(res => res.json())
+            .then(items => {
+                this.setState({ items })
+            });
+    }
+
+
+    inputChange(event) {
+        this.setState({ input: event.target.value });
+    }
+
+    render() {
+        return ( 
+        <div className = "App">
+            <h1> To do list: </h1><br/>
+            <input type = "text"
+                value = { this.state.input }
+                ref = "text"
+                name = "todo"
+                onChange = { this.inputChange }
+                placeholder = "Enter your task" />
+                <br/>
+            <input type = "submit"
+                value = "Submit" />  
+            <ul> 
+            {
+            this.state.items.map(item =>
+                <div key = { item.id }> <li> { item.name } </li></div> )
+                }   
+            </ul>
+        </div>
+        );
+    }
+
+
 }
 
 export default App;
