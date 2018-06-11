820e777d2d2283810db295cd7e50f06b140e1d39
Quinlan Deer <quinlan.deer@gmail.com>
2018-06-11 17:47:13 -0400

    refactor code & fix react key warning

--- src/App.js
+++ src/App.js
@@ -2,70 +2,74 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            input: '',
-            items: []
-        }
-
-        this.inputChange = this.inputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      items: []
     }
 
-    componentDidMount() {
-        fetch('/tasks')
-            .then(res => res.json())
-            .then(items => {
-                this.setState({ items })
-            });
-    }
-
-    handleSubmit() {
-      var params = { name: this.state.input };
-      fetch('/tasks', {
-              method: 'POST',
-              body: JSON.stringify(params),
-
-              headers: {
-                  'Accept': 'application/json',
-                  'Content-Type': 'application/json'
-              }
-          })
-          .then(response => response.json())
-          .then(response => {
-              this.setState({ items: response,
-              input: '' })
-          })
+    this.inputChange = this.inputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-    inputChange(event) {
-        this.setState({ input: event.target.value });
-    }
+  componentDidMount() {
+    fetch('https://qdeer-decode-test.herokuapp.com/')
+      .then(res => res.json())
+      .then(items => {
+        this.setState({ items })
+      });
+  }
 
-    render() {
-        return ( 
-        <div className = "App">
-            <h1> To do list: </h1><br/>
-            <input type = "text"
-                value = { this.state.input }
-                ref = "text"
-                name = "todo"
-                onChange = { this.inputChange }
-                placeholder = "Enter your task" />
-                <br/>
-            <input type = "submit"
-                value = "Submit"
-                onClick = { this.handleSubmit }/>   />  
-            <ul> 
-            {
-            this.state.items.map(item =>
-                <div key = { item.id }> <li> { item.name } </li></div> )
-                }   
-            </ul>
-        </div>
-        );
-    }
+  handleSubmit() {
+    var params = { name: this.state.input };
+    fetch('https://qdeer-decode-test.herokuapp.com/', {
+      method: 'POST',
+      body: JSON.stringify(params),
+
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          items: response,
+          input: ''
+        })
+      })
+  }
+
+  inputChange(event) {
+    this.setState({ input: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> To do list: </h1><br />
+        <input type="text"
+          value={this.state.input}
+          ref="text"
+          name="todo"
+          onChange={this.inputChange}
+          placeholder="Enter your task" />
+        <br />
+        <input type="submit"
+          value="Submit"
+          onClick={this.handleSubmit} />
+        <ul>
+          {
+            this.state.items.map((item, i) =>
+              <div key={i}>
+                <li> {item.name} </li>
+              </div>)
+          }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
